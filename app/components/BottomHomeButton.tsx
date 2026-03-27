a2076704e9838fc22f78c971ae4a1bb9ab4598ff
509sfsrideapp <509sfsrideapp@gmail.com>
2026-03-27 17:58:57 -0500

    Hide bottom home button on home page

--- app/components/BottomHomeButton.tsx
+++ app/components/BottomHomeButton.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function BottomHomeButton() {
+  const pathname = usePathname();
+
+  if (pathname === "/") {
+    return null;
+  }
+
   return (
     <Link
       href="/"
